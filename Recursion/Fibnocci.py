b17bbe41cc2ed67f772d7fb202184f5341fa2b78
Sivanimalladi1 <malladisivani416@gmail.com>
2025-06-06 13:24:38 +0530

    added fibnocci program

---
+++ Recursion/Fibnocci.py
@@ -0,0 +1,12 @@
+def myfunc(n):
+    if(n==1):
+        return 1
+    if(n<0):
+        return 0
+    ans = myfunc(n-1) + myfunc(n-2)
+    return ans
+        
+if __name__ == "__main__":
+    n=4
+    ans = myfunc(n)
+    print(ans)
